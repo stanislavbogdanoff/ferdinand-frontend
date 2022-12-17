50609d9af884cce164651b602c5063dc97142ef3
Ирина Abc <Stanislav Tiryoshin>
2022-12-17 11:15:21 +0600

    update of netlify deploy

--- _components/pagewrapper.tsx
+++ _components/pagewrapper.tsx
@@ -11,7 +11,7 @@ export default function PageWrapper({
 }) {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    setTimeout(() => setLoading(false), 2000);
+    setTimeout(() => setLoading(false), 1500);
   }, []);
   return (
     <>
